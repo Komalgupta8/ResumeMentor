fe473630948219cb2149ddb661696a09eb8815bd
Komalgupta8 <komalguptagupta234@gmail.com>
2025-05-17 14:09:58 +0530

    sec

--- Frontend/src/components/Contact/Contact.jsx
+++ Frontend/src/components/Contact/Contact.jsx
@@ -11,19 +11,19 @@ const Contact = () => {
       <p>If you have any questions, feel free to reach out to me!</p>
 
       <div className="social-links">
-        <a href="https://www.instagram.com/raman_kishore_" target="_blank" rel="noopener noreferrer" className="social-link">
+        <a href="https://www.instagram.com/komalgupta234_" target="_blank" rel="noopener noreferrer" className="social-link">
           <i className='bx bxl-instagram' style={{ color: '#E1306C' }}></i> Instagram
         </a>
 
-        <a href="https://www.linkedin.com/in/raman-kishore08/" target="_blank" rel="noopener noreferrer" className="social-link">
+        <a href="https://www.linkedin.com/in/komalgupta8/" target="_blank" rel="noopener noreferrer" className="social-link">
           <i className='bx bxl-linkedin' style={{ color: '#0A66C2' }}></i> LinkedIn
         </a>
 
-        {/* <a href="https://github.com/RamanKishore08" target="_blank" rel="noopener noreferrer" className="social-link">
+        {/* <a href="" target="_blank" rel="noopener noreferrer" className="social-link">
           <i className='bx bxl-github' style={{ color: '#ffffff' }}></i> GitHub
         </a> */}
 
-        <a href="mailto:ramankishore08@email.com" target="_blank" rel="noopener noreferrer" className="social-link">
+        <a href="mailto:komalguptagupta234@gmail.com" target="_blank" rel="noopener noreferrer" className="social-link">
           <i className='bx bxl-gmail' style={{ color: '#D14836' }}></i> Email
         </a>
       </div>
